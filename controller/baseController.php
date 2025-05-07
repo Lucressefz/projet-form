8e621cf9986dfe67ef3c352f6777f935170a8146
Lucressefz <fortunezeufack@yahoo.com>
2025-05-07 11:17:23 +0200

    base

--- controller/baseController.php
+++ controller/baseController.php
@@ -1,7 +1,7 @@
 <?php
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        # Si la checkbox est cochée, et les autres champs sont remplies : true sinon, false
+       
         $admin = isset($_POST['admin']) ? 1 : 0;
 
     
